94e8634d6da9b5d0c56c6489cf926b75cf33f6af
Adia Stansbury <astansbury@patientslikeme.com>
2018-03-28 13:54:41 -0400

    Removes test blueprint
    Blueprint test fails because of its syntax. Removes it since it is a blueprint.

--- test/threeman_test.rb
+++ test/threeman_test.rb
@@ -4,8 +4,4 @@ class ThreemanTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Threeman::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
